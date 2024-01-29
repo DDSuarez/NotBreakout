5015c4f380ccb27aefbf96a18b9a25110f86eccd
Danny Suarez <metasaval@gmail.com>
2024-01-29 13:18:08 -0800

    added ball respawn, main menu, and end screen

--- src/scripts/game.gd
+++ src/scripts/game.gd
@@ -10,18 +10,34 @@ var ballInstance
 func _ready():
 	$LivesCounter.set_text("Lives: " + str(Globals.lives))
 	
-	brickInstance = brickScene.instantiate()
-	$Bricks.add_child(brickInstance)
+	build_map()
 	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Globals.lives <= 0:
+		get_tree().change_scene_to_file("res://src/scenes/end_screen.tscn")
+		
+	if $Balls.get_child_count() == 0:
+		spawn_ball()
+		
+		
+func spawn_ball():
 	ballInstance = ballScene.instantiate()
 	$Balls.add_child(ballInstance)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func spawn_brick():
+	brickInstance = brickScene.instantiate()
+	$Bricks.add_child(brickInstance)
+	
 
+func build_map():
+	spawn_brick()
 
 func _on_bottom_body_entered(body):
 	Globals.lives -= 1
 	$LivesCounter.set_text("Lives: " + str(Globals.lives))
+	
+	body.queue_free()
+	
