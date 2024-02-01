6e23cd0cefddb18a35b5f7b485713ddeadfda2b9
Danny Suarez <metasaval@gmail.com>
2024-01-31 21:44:53 -0800

    added brick sprite, built map, and made ball faster each hit

--- src/scripts/ball.gd
+++ src/scripts/ball.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
-const SPEED = 600
+const START_SPEED = 600
+const ACCEL : int = 50
+
+var speed : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	speed = START_SPEED
 	velocity = Vector2(randf_range(-1, 1), randf_range(0, 1))
 
 
@@ -13,11 +17,16 @@ func _process(delta):
 	
 
 func _physics_process(delta):
-	var collision = move_and_collide(velocity * SPEED * delta)
+	var collision = move_and_collide(velocity * speed * delta)
 	var collider
 	if collision:
 		$AudioStreamPlayer2D.play()
 		collider = collision.get_collider()
-		velocity = velocity.bounce(collision.get_normal())
-		if collider == $"../../Bricks/Brick":
+		if collider == $"../../Player":
+			speed += ACCEL
+			velocity = velocity.bounce(collision.get_normal())
+		else:
+			velocity = velocity.bounce(collision.get_normal())
+		#print(collider.name)
+		if collider.name.match("Brick*"):
 			collider.queue_free()
